9d43d7a9fb462a8c48917c5d887d59ae6bc4aa2a
Dasom1008 <74761523+Dasom1008@users.noreply.github.com>
2021-01-08 15:21:36 +0900

    (다솜)공지사항 기능 구현중 / 관리자 페이지 메인구현

--- src/main/java/com/ilgusi/notice/controller/NoticeController.java
+++ src/main/java/com/ilgusi/notice/controller/NoticeController.java
@@ -1,14 +1,24 @@
 package com.ilgusi.notice.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.ilgusi.notice.model.service.NoticeService;
 import com.ilgusi.notice.model.vo.Notice;
+
+import common.FileNameOverlap;
 
 @Controller
 public class NoticeController {
@@ -21,6 +31,58 @@ public class NoticeController {
 		ArrayList<Notice> list = service.selectNoticeList();
 		model.addAttribute("list", list);
 		return "notice/noticeList";
+	}
+	
+	//공지사항 작성
+	@RequestMapping("/noticeWriteFrm.do")
+	public String noticeWriteFrm () {
+		return "notice/noticeWriteFrm";
+	}
+	
+	
+	@RequestMapping("/insertNotice.do")
+	public String insertNotice(MultipartHttpServletRequest mRequest, Model model, HttpServletRequest request) {
+		String root = request.getSession().getServletContext().getRealPath("/");
+		String path = root+"upload/notice/";
+		System.out.println("파일 경로 : " + path);
+		
+		MultipartFile file = mRequest.getFile("filename");
+		String filename = file.getOriginalFilename();
+		String filepath = new FileNameOverlap().rename(path, filename);
+		
+		byte[] bytes;
+		try {
+			bytes = file.getBytes();
+			File upFile = new File(path+filepath);
+			FileOutputStream fos = new FileOutputStream(upFile);
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			bos.write(bytes);
+			bos.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Notice n= new Notice();
+		n.setFilename(filename);
+		n.setFilepath(filepath);
+		n.setNTitle(request.getParameter("nTitle"));
+		n.setNContent(request.getParameter("nContent"));
+		int result = service.insertNotice(n);
+		if(result > 0 ) {
+			model.addAttribute("msg","등록되었습니다.");
+		}else {
+			model.addAttribute("msg","등록실패.");
+		}
+		model.addAttribute("loc", "/noticeList.do");
+		return "common/msg";
+	}
+	
+	//공지사항 내용 보기
+	@RequestMapping("/noticeView.do")
+	public String noticeView () {
+		return "";
 	}
 	
 }
