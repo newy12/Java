e4b8bb49788b41597fb594d737f28e916103c031
youngjae <newy12@naver.com>
2022-10-18 23:28:27 +0900

    join 벨리데이션 추가

--- src/main/java/com/summar/summar/results/BooleanResult.java
+++ src/main/java/com/summar/summar/results/BooleanResult.java
@@ -4,13 +4,17 @@ import com.summar.summar.dto.ApiResponseMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 @Slf4j
 public class BooleanResult {
 
-    public static ResponseEntity<?> build(String resultName, boolean resultStatus) {
+    public static ResponseEntity<?> build(String resultName,boolean resultStatus,  String message , List<String> messageResult) {
 
         ApiResult apiResult = ApiResult.blank()
-                .add(resultName, resultStatus);
+                .add(resultName, resultStatus)
+                        .add(message,messageResult);
+
             log.info("test");
         return Result.ok(new ApiResponseMessage(apiResult));
     }
--- src/main/java/com/summar/summar/service/UserService.java
+++ src/main/java/com/summar/summar/service/UserService.java
@@ -1,32 +1,51 @@
 package com.summar.summar.service;
 
+import com.summar.summar.common.SummarCommonException;
+import com.summar.summar.common.SummarErrorCode;
 import com.summar.summar.domain.User;
-import com.summar.summar.dto.ApiResponseMessage;
 import com.summar.summar.dto.JoinRequestDto;
 import com.summar.summar.repository.UserRepository;
-import com.summar.summar.results.ApiResult;
 import com.summar.summar.results.BooleanResult;
-import com.summar.summar.results.Result;
+import com.summar.summar.util.SHA256Util;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor
+@Slf4j
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final SHA256Util sha256Util;
+
+    List<String> list = new ArrayList<>();
 
     @Transactional
-    public void saveUser(JoinRequestDto joinRequestDto) {
-        userRepository.save(new User(joinRequestDto));
-    }
-
-    public ApiResult validation(JoinRequestDto joinRequestDto) {
-        if(joinRequestDto.getUserId() == ""){
-            return ApiResult.message("아이디를 입력해주세요");
+    public SummarCommonException saveUser(JoinRequestDto joinRequestDto) throws NoSuchAlgorithmException {
+        List<User> userInfoList = userRepository.findAll();
+        if (userInfoList.isEmpty()){
+            return new SummarCommonException(SummarErrorCode.USER_NOT_FOUND.getCode(), SummarErrorCode.USER_NOT_FOUND.getMessage());
         }
+        for (User user : userInfoList) {
+            if(user.getUserId().equals(joinRequestDto.getUserId()) || user.getUserNickname().equals(joinRequestDto.getUserNickname())){
+                log.info("아이디 혹은 닉네임이 중복 됩니다.");
+                return new SummarCommonException(SummarErrorCode.USER_NOT_FOUND.getCode(), SummarErrorCode.USER_NOT_FOUND.getMessage());
+            }
+        }
+        //passwordEncoder 양방향 암호화 알고리즘 적용
+        joinRequestDto.setUserHpNo(passwordEncoder.encode(joinRequestDto.getUserHpNo()));
+        //SHA256 단방향 암호화 알고리즘 적용
+        //TODO 다시 구현
+        joinRequestDto.setUserHpNo(sha256Util.encrypt(joinRequestDto.getUserPwd()));
+        userRepository.save(new User(joinRequestDto));
         return null;
     }
 }
