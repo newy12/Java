889ff049144cae2afc787af6c470936658da6e81
yjkim-mtn <yjkim@mtn.co.kr>
2022-12-19 17:56:53 +0900

    빠진 트랜잭션 어노테이션 추가

--- src/main/java/com/summar/summar/service/BannerService.java
+++ src/main/java/com/summar/summar/service/BannerService.java
@@ -5,6 +5,7 @@ import com.summar.summar.repository.BannerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Slf4j
 public class BannerService {
     private final BannerRepository bannerRepository;
+    @Transactional(readOnly = true)
     public List<Banner> findAllByBanner() {
         return bannerRepository.findAll();
     }
