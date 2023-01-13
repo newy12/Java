7459e2adf3dfff1adeb76ec020efa2660386b4de
yjkim-mtn <yjkim@mtn.co.kr>
2023-01-13 16:59:48 +0900

    푸시알람 apiㅇ추가

--- src/main/java/com/summar/summar/controller/SettingController.java
+++ src/main/java/com/summar/summar/controller/SettingController.java
@@ -8,7 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.webjars.NotFoundException;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,17 +22,20 @@ public class SettingController {
 
 
     /**
-     * 공지사항
+     * 설정 페이지
      */
-    @GetMapping("/notice")
-    public ResponseEntity<?> notice() {
-       return ListResult.build("results",settingRepository.findAllBySettingType(SettingType.NOTICE));
+    @GetMapping("")
+    public ResponseEntity<?> setting(@RequestParam(value = "status")String status) {
+        if("notice".equals(status)){
+            return ListResult.build("results",settingRepository.findAllBySettingType(SettingType.NOTICE));
+        }
+        if("question".equals(status)){
+            return ListResult.build("results",settingRepository.findAllBySettingType(SettingType.QUESTION));
+        }
+        else{
+            throw new NotFoundException("없는 타입입니다.");
+        }
+
     }
-    /**
-     * 자주 묻는 질문
-     */
-    @GetMapping("/question")
-    public ResponseEntity<?> question() {
-        return ListResult.build("results",settingRepository.findAllBySettingType(SettingType.QUESTION));
-    }
+
 }
