1069f88f85f5c7a44baeed01fb1899a5eace92ef
ChaeyunMaeng <aodcodbs@gmail.com>
2023-01-16 16:35:26 +0900

    피드 등록 수정

--- src/main/java/com/summar/summar/controller/FeedController.java
+++ src/main/java/com/summar/summar/controller/FeedController.java
@@ -29,55 +29,13 @@ public class FeedController {
 
     @Operation(summary = "피드 등록")
     @PostMapping(value = "", consumes = {"multipart/form-data"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "정상 처리", content = @Content(examples = @ExampleObject(value = "{\n" +
-                    "    \"status\": \"SUCCESS\",\n" +
-                    "    \"message\": \"정상처리\",\n" +
-                    "    \"errorMessage\": null,\n" +
-                    "    \"errorCode\": null,\n" +
-                    "    \"result\": {\n" +
-                    "                \"feedSeq\": 1,\n" +
-                    "                \"userSeq\": 1,\n" +
-                    "                \"contents\": \"dd\",\n" +
-                    "                \"feedImages\": [{\n" +
-                    "                       \"feedImageSeq\": 1,\n" +
-                    "                       \"feedSeq\": 1,\n" +
-                    "                       \"imageUrl\": 1,\n" +
-                    "                       \"orderNo\": 1,\n" +
-                    "                   }]\n" +
-                    "                \"secretYn\": true,\n" +
-                    "                \"commentYn\": true,\n" +
-                    "                \"tempSaveYn\": true,\n" +
-                    "            }"))),
-            @ApiResponse(responseCode = "403", description = "권한 없음(다른 회원의 계정 변경)", content = @Content(examples = @ExampleObject(value = "\"result\":null"))),
-    })
+    @ApiResponse(responseCode = "403", description = "권한 없음(다른 회원의 계정 변경)", content = @Content(examples = @ExampleObject(value = "\"result\":null")))
     public ResponseEntity<?> registFeed(@ModelAttribute FeedRegisterDto feedRegisterDto) {
         return ObjectResult.build("result", feedService.saveFeed(feedRegisterDto));
     }
 
     @Operation(summary = "피드 조회")
     @GetMapping(value = "")
-   /* @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "정상 처리", content = @Content(examples = @ExampleObject(value = "{\n" +
-                    "    \"status\": \"SUCCESS\",\n" +
-                    "    \"message\": \"정상처리\",\n" +
-                    "    \"errorMessage\": null,\n" +
-                    "    \"errorCode\": null,\n" +
-                    "    \"result\": {\n" +
-                    "        \"result\": [\n" +
-                    "            {\n" +
-                    "                \"feedSeq\": 1,\n" +
-                    "                \"userSeq\": 1,\n" +
-                    "                \"contents\": \"dd\",\n" +
-                    "                \"imageUrls\": []\n" +
-                    "                \"commentYn\": \"dd\",\n" +
-                    "            }\n" +
-                    "            ]\n" +
-                    "     }\n"
-                ))),
-
-            @ApiResponse(responseCode = "403", description = "권한 없음(다른 회원의 계정 변경)", content = @Content(examples = @ExampleObject(value = "\"result\":null"))),
-    })*/
     @ApiResponse(responseCode = "403", description = "권한 없음(다른 회원의 계정 변경)", content = @Content(examples = @ExampleObject(value = "\"result\":null")))
     public ResponseEntity<List<FeedDto>> getFeed() {
         return ResponseEntity.ok().body(feedService.getFeed());
