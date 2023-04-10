b68cc1711d94b6b8797cfabb09f3b7fc72b131ed
yjkim-mtn <newy12@naver.com>
2023-04-10 15:38:44 +0900

    JPA 에서 인서트 쿼리 성능 개선

--- insertQueryInJPA/src/main/java/com/example/insertquerytestinjpa/InsertQueryTestInJpaApplication.java
+++ insertQueryInJPA/src/main/java/com/example/insertquerytestinjpa/InsertQueryTestInJpaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InsertQueryTestInJpaApplication {
 
     public static void main(String[] args) {
+        //insertquery
         SpringApplication.run(InsertQueryTestInJpaApplication.class, args);
     }
 
